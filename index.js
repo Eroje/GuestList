cb1c19e70dacaeb2b42a03b9985ff0dfd3b34b5b
Mojid Eroje <93765911+Eroje@users.noreply.github.com>
2023-02-18 02:43:59 +0000

    Added guestList

---
+++ index.js
@@ -0,0 +1,10 @@
+var guestName = prompt("What is your name?");
+
+var guestList = ["Angela", "Tonia","Judith", "Quincy", "Mojid"];
+
+if (guestList.includes(guestName)) {
+     alert ("Welcome!");
+  
+  } else {
+    alert ("Maybe next time!");
+}
